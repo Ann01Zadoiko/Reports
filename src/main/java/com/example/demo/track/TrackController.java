8bb9d23cee53c1aa01ec9ca569d50e88db8b4bf4
Anna <anna.zadoiko@gmail.com>
2025-01-03 10:59:14 +0200

    qq

--- src/main/java/com/example/demo/track/TrackController.java
+++ src/main/java/com/example/demo/track/TrackController.java
@@ -2,13 +2,17 @@ package com.example.demo.track;
 
 
 import com.example.demo.importData.ImportTracks;
+import com.example.demo.tram.Tram;
+import com.example.demo.tram.TramService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Controller
@@ -18,6 +22,7 @@ public class TrackController {
 
     private final ImportTracks importTracks;
     private final TrackService trackService;
+    private final TramService tramService;
 
     //show a page for uploading a file
     @GetMapping
@@ -50,5 +55,34 @@ public class TrackController {
         return "redirect:/v1/tracks";
     }
 
+    @GetMapping("/add")
+    public String add(){
+        return "/tracks/add";
+    }
+
+    //add new user
+    @PostMapping("/add")
+    public String save(@RequestParam("numberOfTram") String numberOfTram,
+                       @RequestParam("depo") String depo,
+                       @RequestParam("day") LocalDate day,
+                       @RequestParam("firstPart") String firstPart,
+                       @RequestParam(value = "time", required = false)  @DateTimeFormat(pattern = "HH:mm") LocalTime time,
+                       @RequestParam("secondPart") String secondPart
+                       ){
+
+        Tram tram = tramService.getByDepoAndNumberOfTram(depo, numberOfTram);
+        Track track = new Track();
+        track.setDay(day);
+        track.setTram(tram);
+        track.setFirstPart(firstPart);
+
+        if (time != null){
+            track.setTime(time);
+            track.setSecondPart(secondPart);
+        }
+
+        trackService.add(track);
+        return "redirect:/v1/tracks";
+    }
 
 }
